ae672b5130518440d526667463a40be90bfb3cc8
mariana de azevedo <mariana.a2006@aluno.ifsc.edu.br>
2025-06-05 11:47:07 -0300

    Parte 1 da aula

---
+++ PDS2-I6-25-1-main/PDS2-I6-25-1-main/LojaMVC/src/main/java/model/Cliente.java
@@ -0,0 +1,84 @@
+
+package model;
+
+import java.sql.Date;
+
+public class Cliente {
+    
+    private int id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private Date dataNascimento;
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+   
+}
